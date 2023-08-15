5a619a0e83ed911f6ad813db50b6f5a3b9fe0b02
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-08-15 09:51:52 +0000

    Edited the kick code and added more error handlers.

--- src/moderationBot/src/kick.js
+++ src/moderationBot/src/kick.js
@@ -11,12 +11,16 @@ $kick[$guildID;$get[user];$messageSlice[1]]
   
 $onlyIf[$rolePosition[$userHighestRole[$authorId;$guildID;id]]<$rolePosition[$userHighestRole[$get[user];$guildID;id]];{newEmbed:{description:Given user has higher position than you. Cannot kick!}{color:Red}}]
 
+$onlyIf[$rolePosition[$userHighestRole[$clientID;$guildID;id]]<$rolePosition[$userHighestRole[$get[user];$guildID;id]];{newEmbed:{description:Given user has higher position than me. Cannot kick!}{color:Red}}]
+
 $onlyIf[$authorID!=$get[user];{newEmbed:{description:You cannot kick yourself.}{color:Red}}] 
 
 $onlyIf[$get[user]!=;{newEmbed:{description:Please mention or provide id of user you want to kick.}{color:Red}}]
   $onlyIf[$hasPerms[$guildId;$authorID;kickmembers]==true;{newEmbed:{description:You need \`Kick Members\` permission to do so.}{color:Red}}]
 
+$onlyIf[$hasPerms[$guildId;$clientID;kickmembers]==true;{newEmbed:{description:I do not have \`Kick Members\` permission in order to kick people.}{color:Red}}
 
 $let[user;$findMember[$message[1];false]]
+$suppressErrors[Something went wrong!]
   `
 }
