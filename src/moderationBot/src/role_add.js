c8f04a6b9b1039b2056287e126e7e3edfbdf4b1a
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-08-15 12:31:43 +0000

    New command - role add

---
+++ src/moderationBot/src/role_add.js
@@ -0,0 +1,41 @@
+module.exports = [{
+    name: "role",
+    category: "moderation",
+    usage: "{prefix} @user @role",
+    code: `
+$sendMessage[{newEmbed:{description:<@&$get[role]> has been added to <@$get[user]>}{timestamp}{color:Green}{footer:Moderator#COLON# $username:$authorAvatar}}]
+    
+$giveRole[$guildID;$get[user];$get[role];Role given by $username]
+    
+    
+$onlyIf[$rolePosition[$userHighestRole[$authorID]]<=$rolePosition[$userHighestRole[$get[user]]];{newEmbed:{description: Given user has higher role than you.}{color:Red}}]
+    
+$onlyIf[$rolePosition[$userHighestRole[$clientID]]<$rolePosition[$get[role]];{newEmbed:{description:The role you wanna me to assign has higher position than my highest role.}{color:Red}}]
+    
+$onlyIf[$rolePosition[$userHighestRole[$authorID]]<$rolePosition[$get[role]];{newEmbed:{description:The role you wanna me to assign has higher position than you.}{color:Red}}]
+    
+$onlyIf[$hasRoles[$guildID;$get[user];$get[role]]==false;{newEmbed:{description:The user already has the given role.}{color:Red}}]
+    
+$onlyIf[$isRoleManaged[$get[role]]==false;{newEmbed:{description:The role given is managed. You cannot add this role to anyone}{color:Red}}]
+
+
+$onlyIf[$get[role]!=;{newEmbed:{description:Please mention or provide id of the role you want to be added to the given user.}{color:Red}}]
+
+$onlyIf[$get[user]!=;{newEmbed:{description:Please mention or provide id of user you wanna add roles to.}{color:Red}}]
+    
+$onlyIf[$memberExists[$get[user]]==true;{newEmbed:{description:The entered user doesn't exists within the server}{color:Red}}]
+    
+$onlyIf[$roleExists[$get[role]]==true;{newEmbed:{description:The entered role doesn't exists within the server}{color:Red}}]
+    
+$onlyIf[$hasPerms[$guildID;$clientID;manageroles]==true;I do not have \`MANAGE_ROLES\` permission.]
+    
+$onlyPerms[manageroles;You do not have \`MANAGE_ROLES\` permission.]
+    
+$let[role;$findRole[$message[3]]]
+    
+$let[user;$findMember[$message[2];false]]
+    
+$suppressErrors[Something went wrong!]
+    
+$onlyIf[$message[1]==$toLowerCase[add];]`
+}]
