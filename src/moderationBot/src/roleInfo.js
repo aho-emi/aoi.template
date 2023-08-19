9a416c7d73e8df568cfcc47dff74a666e816cf96
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-08-19 02:12:50 +0000

    Edited roleInfo.js
    Added hoisted? Position? Editable?

--- src/moderationBot/src/roleInfo.js
+++ src/moderationBot/src/roleInfo.js
@@ -5,7 +5,7 @@ module.exports = [{
     category: "moderation",
     code: `
 
-$sendMessage[{newEmbed:{title:Role info}{description:**Name#COLON#** $roleName[$get[role]] (<@&$get[role]>)\n**Color#COLON#** $get[color]\n**Members Count#COLON#** $roleMembersCount[$get[role]]\n**Created on#COLON#** <t:$truncate[$divide[$creationDate[$get[role];ms];1000]]:f>}{footer:ID#COLON# $get[role]}{color:$get[color]}}]
+$sendMessage[{newEmbed:{title:Role info}{description:**Name#COLON#** $roleName[$get[role]] (<@&$get[role]>)\n**Color#COLON#** $get[color]\n**Members Count#COLON#** $roleMembersCount[$get[role]]\n**Created on#COLON#** <t:$truncate[$divide[$creationDate[$get[role];ms];1000]]:f>\n**Position#COLON#** $rolePosition[$get[role]]\n**Hoisted?** $toLocaleUpperCase[$isHoisted[$get[role]]]\n**Editable?** $toLocaleUpperCase[$isRoleEditable[$get[role]]]}{footer:ID#COLON# $get[role]}{color:$get[color]}}]
 
 $onlyIf[$get[role]!=;{newEmbed:{description:Please mention or provide id of the role you want to be added to the given user.}{color:Red}}]
 
@@ -16,5 +16,6 @@ $let[color;$getRoleColor[$get[role]]]
 $let[role;$findRole[$message[1]]]
 
 $suppressErrors[Something went wrong!]
+
 `
 }]
