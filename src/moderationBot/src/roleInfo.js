1d0f980949c969814cda702c1945d15cbdd1ad49
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2023-08-19 01:27:48 +0000

    Added Role info command
    Role info cmd

---
+++ src/moderationBot/src/roleInfo.js
@@ -0,0 +1,20 @@
+module.exports = [{
+    name: "role info",
+    description: "Gives information about a role.",
+    usage: "{prefix} role info @role",
+    category: "moderation",
+    code: `
+
+$sendMessage[{newEmbed:{title:Role info}{description:**Name#COLON#** $roleName[$get[role]] (<@&$get[role]>)\n**Color#COLON#** $get[color]\n**Members Count#COLON#** $roleMembersCount[$get[role]]\n**Created on#COLON#** <t:$truncate[$divide[$creationDate[$get[role];ms];1000]]:f>}{footer:ID#COLON# $get[role]}{color:$get[color]}}]
+
+$onlyIf[$get[role]!=;{newEmbed:{description:Please mention or provide id of the role you want to be added to the given user.}{color:Red}}]
+
+$onlyIf[$roleExists[$get[role]]==true;{newEmbed:{description:The entered role doesn't exists within the server.}{color:Red}}]
+
+$let[color;$getRoleColor[$get[role]]]
+
+$let[role;$findRole[$message[1]]]
+
+$suppressErrors[Something went wrong!]
+`
+}]
